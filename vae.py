a04da7daf85bdc1494adb8eae5bc28dc84e7ee74
Yue Jin <yj2360@columbia.edu>
2017-12-10 16:16:05 -0500

    updated plotting function

--- vae.py
+++ vae.py
@@ -272,25 +272,32 @@ network_architecture =     dict(n_hidden_recog_1=500, # 1st layer encoder neuron
          n_input=1024, # 
          n_z=20)  # dimensionality of latent space
 
-vae = train(network_architecture, training_epochs=75)
+vae = train(network_architecture, training_epochs=10)
 
 
 # Based on this we can sample some test inputs and visualize how well the VAE can reconstruct those. In general the VAE does really well.
 
 # In[7]:
 
-x_sample = dataset.next_test_batch(100)[:,:,:,1][0]
+play = dataset.next_test_batch(100).reshape(100,1024,3)
+x_sample = play[:,:,1][2,].reshape(1,1024)
+plt.imshow(x_sample.reshape(32, 32), vmin=0, vmax=1, cmap="gray")
 x_reconstruct = vae.reconstruct(x_sample)
+
+
+x_sample1 = play[2]
+plt.imshow(x_sample1.reshape(32, 32,3), vmin=0, vmax=1)
+
 
 plt.figure(figsize=(8, 12))
 for i in range(5):
 
     plt.subplot(5, 2, 2*i + 1)
-    plt.imshow(x_sample[i].reshape(28, 28), vmin=0, vmax=1, cmap="gray")
+    plt.imshow(x_sample[i].reshape(32, 32), vmin=0, vmax=1, cmap="gray")
     plt.title("Test input")
     plt.colorbar()
     plt.subplot(5, 2, 2*i + 2)
-    plt.imshow(x_reconstruct[i].reshape(28, 28), vmin=0, vmax=1, cmap="gray")
+    plt.imshow(x_reconstruct[i].reshape(32, 32), vmin=0, vmax=1, cmap="gray")
     plt.title("Reconstruction")
     plt.colorbar()
 plt.tight_layout()
@@ -304,17 +311,17 @@ plt.tight_layout()
 
 network_architecture =     dict(n_hidden_recog_1=500, # 1st layer encoder neurons
          n_hidden_recog_2=500, # 2nd layer encoder neurons
-         n_hidden_gener_1=500, # 1st layer decoder neurons
+         n_hidden_gener_1=500, #          st layer decoder neurons
          n_hidden_gener_2=500, # 2nd layer decoder neurons
-         n_input=784, # MNIST data input (img shape: 28*28)
+         n_input=1024, 
          n_z=2)  # dimensionality of latent space
 
-vae_2d = train(network_architecture, training_epochs=75)
+vae_2d = train(network_architecture, training_epochs=10)
 
 
 # In[9]:
 
-x_sample, y_sample = dataset.next_test_batch(5000)[:,:,:,1]
+x_sample = dataset.next_test_batch(5000).reshape(100,1024,3)[:,:,1].reshape(5000,1024)
 z_mu = vae_2d.transform(x_sample)
 plt.figure(figsize=(8, 6)) 
 plt.scatter(z_mu[:, 0], z_mu[:, 1], c=np.argmax(y_sample, 1))
@@ -330,12 +337,14 @@ nx = ny = 20
 x_values = np.linspace(-3, 3, nx)
 y_values = np.linspace(-3, 3, ny)
 
-canvas = np.empty((28*ny, 28*nx))
+
+
+canvas = np.empty((32*ny, 32*nx))
 for i, yi in enumerate(x_values):
     for j, xi in enumerate(y_values):
         z_mu = np.array([[xi, yi]]*vae.batch_size)
         x_mean = vae_2d.generate(z_mu)
-        canvas[(nx-i-1)*28:(nx-i)*28, j*28:(j+1)*28] = x_mean[0].reshape(28, 28)
+        canvas[(nx-i-1)*32:(nx-i)*32, j*32:(j+1)*32] = x_mean[0].reshape(32, 32)
 
 plt.figure(figsize=(8, 10))        
 Xi, Yi = np.meshgrid(x_values, y_values)
